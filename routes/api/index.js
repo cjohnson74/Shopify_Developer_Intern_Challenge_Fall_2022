c0eb5b9668e8cfae89f9ee222a2079a08d6153b3
cjohnson74 <cjohnson74.tech@gmail.com>
2022-05-19 17:44:28 -0400

    added inventoryitms routes to the index file in the api folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const warehouseRoutes = require('./warehouse-routes');
+const inventoryItemRoutes = require('./inventoryItem-routes');
 
 router.use('/warehouses', warehouseRoutes);
+router.use('/inventoryitems', inventoryItemRoutes);
 
 module.exports = router;
