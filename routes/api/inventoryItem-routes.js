577c530d81af987b97bc79d353780dd9fef9d2cb
cjohnson74 <cjohnson74.tech@gmail.com>
2022-05-19 17:25:14 -0400

    added inventoryItem: create route in the inventoryItem-routes file

--- routes/api/inventoryItem-routes.js
+++ routes/api/inventoryItem-routes.js
@@ -18,3 +18,34 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// get one inventoryItem
+router.get('/:id', (req, res) => {
+    // find a single inventoryItem by its 'id'
+    // includes its associated Warehouse
+    InventoryItem.findByPk(req.params.id, {
+        where: {
+            id: req.params.id,
+        },
+        include: [
+            Warehouse,
+        ],
+    })
+        .then((inventoryItem) => res.json(inventoryItems))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
+// create new inventoryItem
+router.post('/', (req, res) => {
+    InventoryItem.create(req.body)
+        .then((inventoryItem) => {
+            res.status(200).json(inventoryItem);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
