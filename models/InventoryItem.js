19b0f0fe0dc8b88646693ee92a9b06613ecfaed9
cjohnson74 <cjohnson74.tech@gmail.com>
2022-05-19 16:49:15 -0400

    created IntventoryItem model

---
+++ models/InventoryItem.js
@@ -0,0 +1,54 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize InvetoryItem model (table) by extending off Sequelize's Model class
+class InventoryItem extends Model {}
+
+// set up fields and rules for InventoryItem model
+InventoryItem.init(
+    {
+        // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        inventoryItem_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        preice: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+            validate: {
+                isDecimal: true,
+            },
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                isNumeric: true,
+            },
+        },
+        warehouse_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'warehouse',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize: sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product',
+    }
+);
+
+module.exports = InventoryItem;
