0bce1d16de3448a4cdffd05548b2c68ef11c8331
cjohnson74 <cjohnson74.tech@gmail.com>
2022-05-21 01:49:11 -0400

    fixed mysql decimal datatype precision

--- models/InventoryItem.js
+++ models/InventoryItem.js
@@ -21,7 +21,7 @@ InventoryItem.init(
             allowNull: false,
         },
         price: {
-            type: DataTypes.DECIMAL,
+            type: DataTypes.DECIMAL(10, 2),
             allowNull: false,
             validate: {
                 isDecimal: true,
