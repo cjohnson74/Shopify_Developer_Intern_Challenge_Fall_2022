d15a0ff099585f05659a17469d3e738e76b305bf
cjohnson74 <cjohnson74.tech@gmail.com>
2022-05-20 21:19:50 -0400

    added create new warehouse form to dashboard view

---
+++ public/js/dashboard.js
@@ -0,0 +1,46 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const location = docuement.querySelector('#warehouse-location').value.trim();
+    const name = document.querySelector('#warehouse-name').value.trim();
+
+    if (location && name) {
+        const response = await fetch('/api/warhouses', {
+            method: 'POST',
+            body: JSON.stringify({ location, name }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to create warehouse');
+        }
+    }
+};
+
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch('/api/warehouses/#{id}', {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to delete warehouse');
+        }
+    }
+};
+
+document
+    .querySelector('.new-warehouse-form')
+    .addEventListener('submit', newFormHandler);
+
+document
+    .querySelector('.warehouse-list')
+    .addEventListener('click', delButtonHandler);
